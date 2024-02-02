da4598ddd9759f0bcc3fdb10cd43ed9dc941fea0
BaastPriest <Aul0na@rambler.ru>
2024-02-02 22:13:05 +0400

    update 8_7_set.py

--- pokoleniye_python_2_lvl/8_set/8_7_set.py
+++ pokoleniye_python_2_lvl/8_set/8_7_set.py
@@ -12,6 +12,7 @@ set1 > set2     set1 >= set2 and set1 != set2
 set1.issuperset(list1) BUT print(set1 >= list1) will be error
 """
 
+
 def is_any_identical_digits_in_two_numbers():
     """YES if the data record contains identical digits and NO if not."""
     set1, set2 = set(input()), set(input())
@@ -20,6 +21,7 @@ def is_any_identical_digits_in_two_numbers():
     else:
         print("YES")
     # print('NO' if set(input()).isdisjoint(set(input())) else 'YES')
+
 
 def is_set1_is_superset_for_set2():
     set1, set2 = set(input()), set(input())
@@ -35,3 +37,27 @@ def print_student_grades_1():
     student_1, student_2, student_3 = set(input().split()), set(input().split()), set(input().split())
     print(*sorted(student_1 & student_2 - student_3, reverse=True, key=int))
 
+
+def print_student_grades_2():
+    """dimensions that occur in no more than two blocks."""
+    set1 = set(int(i) for i in input().split())
+    set2 = set(int(i) for i in input().split())
+    set3 = set(int(i) for i in input().split())
+    print(*sorted((set1 | set2 | set3) - (set1 & set2 & set3)))
+
+
+def print_student_grades_3():
+    """grades of the third student that are not found in either the first or the second student."""
+    set1 = set(int(i) for i in input().split())
+    set2 = set(int(i) for i in input().split())
+    set3 = set(int(i) for i in input().split())
+    print(*sorted(set3 - (set1 | set2), reverse=True))
+
+
+def print_student_grades_4():
+    """grades not found in any of the three students."""
+    all_grades_set = {int(i) for i in range(11)}
+    set1 = set(int(i) for i in input().split())
+    set2 = set(int(i) for i in input().split())
+    set3 = set(int(i) for i in input().split())
+    print(*sorted(all_grades_set - (set1 | set2 | set3)))
